caa58c9a864c61b82bf24d10c1710f9c98c34933
william ali <william.ali@hotmail.se>
2022-08-17 20:22:57 +0200

    Upload todo Project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,171 @@
+<template>
+  <ol>
+    <div class="wrapper" v-for="t in todos" :key="t.id">
+      <li v-if="!t.isCompleted" @click="t.isCompleted = true" class="list-items">
+        <h4 class="title-text">{{ t.title }}</h4>
+        <img class="checkmark checkmark-grayscale" src="../assets/CheckMark.png" width="30" height="30"
+          alt="Checkmark" />
+        <button class="delete-btn" @click="deleteTodo(t.id)"><img src="../assets/cross.jpg" width="30" height="30"
+            alt="cross" /></button>
+      </li>
+      <li v-else @click="t.isCompleted = false" class="list-items list-items-completed">
+        <h4 class="title-text">{{ t.title }}</h4>
+        <img class="checkmark" src="../assets/CheckMark.png" width="30" height="30" alt="Checkmark" />
+        <button class="delete-btn" @click="deleteTodo(t.id)"><img src="../assets/cross.jpg" width="30" height="30"
+            alt="cross" /></button>
+      </li>
+    </div>
+  </ol>
+  <button class="deleteAll-btn" @click="deleteAll()">Ta bort alla</button>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      isCompleted: false,
+      todos: [],
+    }
+  },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  created() {
+    fetch('http://localhost:5113/api/Todo')
+      .then(response => response.json())
+      .then(data => {
+        this.todos = data
+      })
+  },
+  methods: {
+
+    getObjFromApi() {
+      fetch('http://localhost:5113/api/Todo')
+        .then(response => response.json())
+        .then(data => {
+          this.todos = data
+        })
+    },
+
+    deleteTodo(id) {
+      fetch('http://localhost:5113/api/Todo/' + id, {
+        method: 'DELETE'
+      })
+        .then(() => {
+          this.getObjFromApi()
+        })
+    },
+    deleteAll() {
+      this.todos.forEach(todo => {
+        fetch('http://localhost:5113/api/Todo/' + todo.id, {
+          method: 'DELETE'
+        })
+          .then(() => {
+            this.getObjFromApi()
+          })
+      })
+    }
+  },
+}
+
+</script>
+<style scoped>
+ol {
+  display: flex;
+  flex-direction: column;
+}
+
+.wrapper
+{
+  display: list-item;
+}
+
+.list-items
+{
+  display: grid;
+  width: 100%;
+  gap:20px;
+  margin-bottom: 20px;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  grid-template-areas: "title-text checkmark crossmark";
+  cursor: pointer;
+  border: solid black;
+  border-radius: 10px;
+  box-shadow: 3px 4px black;
+}
+
+.list-items-completed
+{
+  text-decoration-line: line-through;
+  text-decoration-thickness: 3px;
+  text-decoration-color: black;
+}
+
+.title-text
+{
+  grid-area: title-text;
+  display: flex;
+  flex-wrap: wrap;
+  width: fit-content;
+  color: #42b983;
+  font-size: 20px;
+  padding-left: 15px;
+}
+
+.checkmark
+{
+  grid-area: checkmark;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+}
+
+.delete-btn
+{
+  grid-area: crossmark;
+  display: flex;
+  justify-content: right;
+  padding-top: 10%;
+  filter: grayscale(100);
+  background-color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.delete-btn:hover
+{
+  filter: grayscale(0);
+}
+
+.deleteAll-btn
+{
+  background: #42b983;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  width: 300px;
+  padding: 20px;
+  font-size: 18px;
+  box-shadow: 3px 4px black;
+}
+
+.deleteAll-btn:hover
+{
+  opacity: .75;
+}
+
+.checkmark-grayscale
+{
+  filter: grayscale(100);
+}
+
+::marker
+{
+  font-size: 30px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <input id="inputId" v-on:keydown.enter="saveTodo()" class="input-field" type="text"
+      placeholder="Vad behöver du göra?" v-model="title" />
+  </div>
+  <div>
+    <HelloWorld :key="todoKey" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'HomeView',
+
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      title: '',
+      todoKey: 0
+    }
+  },
+  methods: {
+    saveTodo() {
+      const todo = {
+        title: this.title
+      }
+      fetch('http://localhost:5113/api/Todo', {
+        method: 'POST',
+        body: JSON.stringify(todo),
+        headers: { 'content-type': 'application/json' },
+      })
+        .then(() => {
+          this.title = ''
+          this.todoKey++
+        })
+    },
+  }
+}
+</script>
+<style scoped>
+.input-field
+{
+  margin-top: 150px;
+  width: 20%;
+  padding-bottom: 20px;
+  padding-top: 6px;
+  padding-left: 10px;
+  border-radius: 4px;
+  box-shadow: 3px 4px;
+}
+
+.input-field:focus
+{
+  outline: none;
+}
+</style>
